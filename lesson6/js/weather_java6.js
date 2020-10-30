719a423ca7d0966e050429a88fa8564766d58f99
alissacorona <cor17003@byui.edu>
2020-10-29 21:48:47 -0600

    update

---
+++ lesson6/js/weather_java6.js
@@ -0,0 +1,17 @@
+var dayOfWeek = new Date().getDay() 
+if (dayOfWeek == 6) {
+document.getElementById("big-banner").style.display = "block";
+}
+const hambutton = document.querySelector('.ham');
+  const mainnav = document.querySelector('.navigation')
+  
+  hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+  </script>
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js">
+  WebFont.load({
+google: {
+families: [
+ 'fontfamilynameofyourchoice'
+]
+}
+});
