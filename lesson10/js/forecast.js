8118e2702b420d8be926d43f4cec75aa7bb574af
alissacorona <cor17003@byui.edu>
2020-11-21 20:12:26 -0700

    update

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -7,11 +7,8 @@ fetch(apiURLforecast)
         const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
         forecast.forEach(x => {
             const d = new Date(x.dt_txt);
-            const imagesrc = `https://openweathermap.org/img/w/${x.weather[0].icon}.png`;
             document.getElementById(`dayofweek${day}`).textContent = weekdays[d.getDay()];
             document.getElementById(`temp${day}`).textContent = Math.round(x.main.temp)
-            document.getElementById(`wicon${day}`).setAttribute("src", imagesrc)
-            document.getElementById(`wicon${day}`).setAttribute("alt", x.weather[0].description)
             day++
         });
 
