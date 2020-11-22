6647d3495e75abe2b597878e82cd8614fd2f0e5d
alissacorona <cor17003@byui.edu>
2020-11-21 19:59:39 -0700

    update

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,16 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=2b7568d348474360f18370d64df7ac87";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = Math.round(jsObject.main.temp);
+    document.getElementById('temp_max').textContent = Math.round(jsObject.main.temp_max);
+    document.getElementById('humidity').textContent = Math.round(jsObject.main.humidity);
+    document.getElementById('wind').textContent = Math.round(jsObject.main.wind);
+    document.getElementById('speed').textContent = Math.round(jsObject.wind.speed);
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+    const desc = jsObject.weather[0].description;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
