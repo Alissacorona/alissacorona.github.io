d97839161d2b225c57b416f2af742cfc9ca167dd
alissacorona <cor17003@byui.edu>
2020-11-28 23:03:05 -0700

    update

---
+++ lesson11/js/forecast.js
@@ -0,0 +1,16 @@
+const apiURLforecast = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=57c6f64b2f9ae9242a51f86539052cf5";
+fetch(apiURLforecast)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        const forecast = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
+        let day = 1
+        const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        forecast.forEach(x => {
+            const d = new Date(x.dt_txt);
+            const imagesrc = `https://openweathermap.org/img/w/${x.weather[0].icon}.png`;
+            document.getElementById(`dayofweek${day}`).textContent = weekdays[d.getDay()];
+            document.getElementById(`temp${day}`).textContent = Math.round(x.main.temp)
+            day++
+        });
+
+    });
---
+++ lesson11/js/gallery.js
@@ -0,0 +1,9 @@
+var dayOfWeek = new Date().getDay() 
+if (dayOfWeek == 6) {
+document.getElementById("big-banner").style.display = "block";
+}
+const hambutton = document.querySelector('.ham');
+  const mainnav = document.querySelector('.navigation')
+  
+  hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+  
