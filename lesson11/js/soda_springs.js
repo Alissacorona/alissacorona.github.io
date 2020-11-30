c14f6afa003c74da9c8e72280fb8c8a90272c687
alissacorona <cor17003@byui.edu>
2020-11-29 19:51:53 -0700

    update

---
+++ lesson11/js/soda_springs.js
@@ -0,0 +1,17 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=57c6f64b2f9ae9242a51f86539052cf5";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = Math.round(jsObject.main.temp);
+    document.getElementById('temp_max').textContent = Math.round(jsObject.main.temp_max);
+    document.getElementById('humidity').textContent = Math.round(jsObject.main.humidity);
+    document.getElementById('wind').textContent = Math.round(jsObject.main.wind);
+    document.getElementById('speed').textContent = Math.round(jsObject.wind.speed);
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+  });
+ 
